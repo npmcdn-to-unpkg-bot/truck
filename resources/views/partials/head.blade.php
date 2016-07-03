f74cbcd50dcb780141060f63af5c2e15fcf01e0c
Emmanuel Odeleye <odeleye.emmanuel@gmail.com>
2016-07-03 16:02:26 -0500

    added google maps api key

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -4,7 +4,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://fonts.googleapis.com/css?family=Roboto:100,400,700" rel="stylesheet">
 	<link rel="stylesheet" href="/css/main.css">
-	<script src="//maps.googleapis.com/maps/api/js"></script>
+	<script src="//maps.googleapis.com/maps/api/js?key=AIzaSyCn5ARfEvG7ivp5u-yX80YZF1DKHd8u7n4"></script>
 	<script src="/js/main.js"></script>
 	@yield('head')
 </head>
