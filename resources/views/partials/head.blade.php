c523e97b70a429e80acf30b10189212ceeeaec49
Emmanuel Odeleye <odeleye.emmanuel@gmail.com>
2016-07-08 23:36:26 -0500

    added basscss

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,7 +3,8 @@
 	<title>Truck - @yield('title')</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://fonts.googleapis.com/css?family=Roboto:100,400,700" rel="stylesheet">
+	<link href="https://npmcdn.com/basscss@7.1.1/css/basscss.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="/css/main.css">
-	  @stack('styles')
+	@stack('styles')
 	@stack('scripts')
 </head>
