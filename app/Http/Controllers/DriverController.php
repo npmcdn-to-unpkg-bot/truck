14e986dd2a848ab69641ccee51b94708f8e91d90
Emmanuel Odeleye <odeleye.emmanuel@gmail.com>
2016-07-11 19:08:47 -0500

    change the way roles are attached

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -60,7 +60,8 @@ class DriverController extends Controller
 	        'password'=> bcrypt($request->input('password')),
 	        'tel' => '+234'.ltrim($request->input('tel'), '0')
 	   ]);
-	   $user->roles()->attach(2);
+	   $role = \App\Role::where('name', '=', 'admin')->firstOrFail()->id;
+	   $user->roles()->attach($role);
 	   Auth::login($user);
     	return view('truck.register')->withSuccess('Your Account was registered successfully');
     }
